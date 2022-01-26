4c95176d1701efebc51b95f2326efc87c599ce44
leoander01 <leoanderneves8@outlook.com>
2022-01-26 08:52:16 -0300

    [C04P05] - Service bootstrap e testando a API manualmente

--- src/server.ts
+++ src/server.ts
@@ -38,4 +38,10 @@ export class SetupServer extends Server {
   public getApp(): Application {
     return this.app;
   }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening of port:', this.port);
+    });
+  }
 }
