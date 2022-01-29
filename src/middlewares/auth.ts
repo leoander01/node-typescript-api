b8c8d3b5a60ca7f46fd7c1f17fa60e013a11567d
leoander01 <leoanderneves8@outlook.com>
2022-01-29 13:03:35 -0300

    [C05P05] - Módulo de Users - Express auth middleware e Typescript module augmentation

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,17 @@
+import AuthService from '@src/services/auth';
+import { NextFunction, Request, Response } from 'express';
+
+export function authMiddleware(
+  req: Partial<Request>,
+  res: Partial<Response>,
+  next: NextFunction
+): void {
+  const token = req.headers?.['x-access-token'];
+  try {
+    const decoded = AuthService.decodedToken(token as string);
+    req.decoded = decoded;
+    next();
+  } catch (err) {
+    res.status?.(401).send({ code: 401, error: err.message });
+  }
+}
--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -18,6 +18,7 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
+        user: 'fake-id',
       },
     ];
 
@@ -102,6 +103,7 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
+        user: 'fake-id',
       },
     ];
 
